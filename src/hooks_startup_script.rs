4a8f1717449d7175a0fedde0aa6015f8f21d65b2
MathisWellmann <wellmannmathis@gmail.com>
2021-09-23 14:57:47 +0200

    init commit

---
+++ src/hooks_startup_script.rs
@@ -0,0 +1,26 @@
+use penrose::{
+    core::{
+        client::Client,
+        data_types::Region,
+        hooks::Hook,
+        manager::WindowManager,
+        xconnection::{XConn, Xid},
+    },
+    draw::{Color, DrawContext, Widget},
+    Result, Selector,
+};
+
+pub struct StartupScript {
+    path: String,
+}
+impl StartupScript {
+    pub fn new(s: impl Into<String>) -> Self {
+        Self { path: s.into() }
+    }
+}
+
+impl<X: XConn> Hook<X> for StartupScript {
+    fn startup(&mut self, _: &mut WindowManager<X>) -> Result<()> {
+        spawn!(&self.path)
+    }
+}
