4272b6ad584186da7a40c0c4a4f605782166cfb2
MathisWellmann <wellmannmathis@gmail.com>
2024-06-21 17:22:27 +0200

    modify `flake.nix`

--- flake.nix
+++ flake.nix
@@ -4,6 +4,10 @@
   inputs = {
     nixpks.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpks";
+    };
     naersk = {
       url = "github:nix-community/naersk";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -14,18 +18,22 @@
     nixpkgs,
     naersk,
     flake-utils,
+    rust-overlay,
     ...
   }:
   flake-utils.lib.eachDefaultSystem (
     system:
      let
+      overlays = [(import rust-overlay)];
       pkgs = import nixpkgs {
-        inherit system;
+        inherit system overlays;
       };
       naerskLib = pkgs.callPackage naersk {};
     in {
       devShells.default = pkgs.mkShell {
-        buildInputs = [];
+        buildInputs = with pkgs; [
+          rust-bin.stable.latest.default
+        ];
       };
       defaultPackage = naerskLib.buildPackage {
         pname = "penrose-personal";
